f27cbfc124e9c927e3fb05bdee16377e85f63fc4
galatiayxh <1538804595@qq.com>
2020-05-29 11:02:55 +0800

    打
    撒旦

--- Http系列/Form1.cs
+++ Http系列/Form1.cs
@@ -1,5 +1,4 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -18,6 +17,19 @@ namespace Http系列
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string strImageURL = "http://test-imaging.oss-cn-hangzhou.aliyuncs.com/Picutre/1730a8b0-3fc1-4c94-8f78-cc53447b5d2d/1366/2020/dcea804c-e51a-40f8-b6a3-bea9e9f20194.png?Expires=3738056270&OSSAccessKeyId=w0hFljLIV1wbGBWX&Signature=4TLiKYxPc0EukRWsjNguWJbXrec%3D";
+
+            System.Net.WebClient webClient = new System.Net.WebClient();
+            webClient.DownloadFile(strImageURL, @"D:\image\1.png");
         }
     }
 }
