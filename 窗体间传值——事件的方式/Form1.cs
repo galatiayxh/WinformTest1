dbd6a391eb47c82d57b605c835e187244e24aaae
galatiayxh <1538804595@qq.com>
2020-07-24 11:31:48 +0800

    窗体间传值__事件的方式

---
+++ 窗体间传值——事件的方式/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 窗体间传值__事件的方式
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form2_changeText(string str)
+        {
+            this.textBox1.Text = str;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.changeText += Form2_changeText;
+            form2.ShowDialog();
+        }
+    }
+}
---
+++ 窗体间传值——事件的方式/Form2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 窗体间传值__事件的方式
+{
+    public partial class Form2 : Form
+    {
+        public delegate void mydel(string str);
+        public event mydel changeText;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string aa = "nihao 事件";
+            changeText(aa);
+        }
+    }
+}
